f60c7d817c93f7c04b5ef3731ac220066ffff5df
NathanVoldman <49371700+NathanVoldman@users.noreply.github.com>
2022-04-30 19:23:40 +0300

    Update filter_words_with_letter.py

--- filter_words_with_letter.py
+++ filter_words_with_letter.py
@@ -1,11 +1,11 @@
-def word_has_letter(x, y):  # has_let # B
+def word_has_letter(x, y):
     for a in x:
         if a.lower() == y.lower():
             return True
     return False
 
 
-def find_words_with_letter(x_list, y):  # wrd_w_let # A
+def find_words_with_letter(x_list, y):
     z = []
     for x in x_list:
         if word_has_letter(x, y):
@@ -17,6 +17,48 @@ if __name__ == "__main__":
     x_list = ['hello', 'are', 'grow', 'spam', 'ham', 'hi', 'go', 'to', 'this']
     y = 'h'
     print(find_words_with_letter(x_list, y))
+
+
+#     def has_let(x, y):
+#         for a in x:
+#             if a.lower() == y.lower():
+#                 return True
+#         return False
+
+
+#     def wrd_w_let(x_list, y):
+#         z = []
+#         for x in x_list:
+#             if has_let(x, y):
+#                 z.append(x)
+#         return z
+
+
+#     if __name__ == "__main__":
+#         x_list = ['hello', 'are', 'grow', 'spam', 'ham', 'hi', 'go', 'to', 'this']
+#         y = 'h'
+#         print(wrd_w_let(x_list, y))
+
+    
+# def B(x, y):
+#     for a in x:
+#         if a.lower() == y.lower():
+#             return True
+#     return False
+
+
+# def A(x_list, y):
+#     z = []
+#     for x in x_list:
+#         if B(x, y):
+#             z.append(x)
+#     return z
+
+
+# if __name__ == "__main__":
+#     x_list = ['hello', 'are', 'grow', 'spam', 'ham', 'hi', 'go', 'to', 'this']
+#     y = 'h'
+#     print(A(x_list, y))
 
 # answers:
 
