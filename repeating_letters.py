9caf158a8e0717f4049d54f34c8154a50ae5a60f
NathanVoldman <49371700+NathanVoldman@users.noreply.github.com>
2022-04-30 19:37:09 +0300

    Update repeating_letters.py

--- repeating_letters.py
+++ repeating_letters.py
@@ -1,4 +1,4 @@
-def has_repeating_letter(x):  # has_rep
+def has_repeating_letter(x):
     x = list(x)
     for i in range(len(x) - 1):
         if x[i] == x[i + 1]:
@@ -6,7 +6,7 @@ def has_repeating_letter(x):  # has_rep
     return False
 
 
-def filter_repeating_letter_words(x_list):  # flt_rep
+def filter_repeating_letter_words(x_list):
     z = []
     for x in x_list:
         if has_repeating_letter(x):
@@ -17,6 +17,46 @@ def filter_repeating_letter_words(x_list):  # flt_rep
 if __name__ == "__main__":
     x_list = ['this', 'riddle', 'has', 'nothing', 'to', 'do', 'with', 'all', 'the', 'other', 'questions']
     print(filter_repeating_letter_words(x_list))
+
+# def has_rep(x):
+#     x = list(x)
+#     for i in range(len(x) - 1):
+#         if x[i] == x[i + 1]:
+#             return True
+#     return False
+
+
+# def flt_rep(x_list):
+#     z = []
+#     for x in x_list:
+#         if has_rep(x):
+#             z.append(x)
+#     return z
+
+
+# if __name__ == "__main__":
+#     x_list = ['this', 'riddle', 'has', 'nothing', 'to', 'do', 'with', 'all', 'the', 'other', 'questions']
+#     print(flt_rep(x_list))
+
+# def B(x):
+#     x = list(x)
+#     for i in range(len(x) - 1):
+#         if x[i] == x[i + 1]:
+#             return True
+#     return False
+
+
+# def A(x_list):
+#     z = []
+#     for x in x_list:
+#         if B(x):
+#             z.append(x)
+#     return z
+
+
+# if __name__ == "__main__":
+#     x_list = ['this', 'riddle', 'has', 'nothing', 'to', 'do', 'with', 'all', 'the', 'other', 'questions']
+#     print(A(x_list))
 
 # Answers:
 
