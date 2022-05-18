d0177106d066ced07a06f6df2386d22902fd66c3
NathanVoldman <49371700+NathanVoldman@users.noreply.github.com>
2022-05-18 15:48:57 +0300

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,5 +1,4 @@
-def multiply(x, y): # mul  # A
-    ''' This function does ??? '''
+def multiply(x, y):
     ans = 0
     for _ in range(y):
         ans += x
@@ -10,3 +9,31 @@ if __name__ == "__main__":
     x, y = 3, 6
     ans = multiply(x, y)
     print(ans)
+
+##########
+   
+# def mul(x, y):
+#     ans = 0
+#     for _ in range(y):
+#         ans += x
+#     return ans
+
+
+# if __name__ == "__main__":
+#     x, y = 3, 6
+#     ans = mul(x, y)
+#     print(ans)
+
+ ##########
+    
+# def A(x, y):
+#     ans = 0
+#     for _ in range(y):
+#         ans += x
+#     return ans
+
+
+# if __name__ == "__main__":
+#     x, y = 3, 6
+#     ans = A(x, y)
+#     print(ans)
